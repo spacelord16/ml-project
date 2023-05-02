1734dc085f82ed7d07956ef4f1775fd781884fab
spacelord16 <deshpande.aditya16@gmail.com>
2023-05-02 11:58:10 +0530

    Updated exception and utils

--- src/excption.py
+++ src/excption.py
@@ -9,7 +9,7 @@ def error_message_detail(error, error_detail:sys):
     
     file_name = exc_tb.tb_frame.f_code.co_filename  # Everything is given in the exception handling documnetation 
     
-    error_message = "Erorr occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno,str(error))
         
     return error_message
